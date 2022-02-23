3c1ace7f7b019cff8130341601893d36e186fb5f
Haguermeister <cadethague70@gmail.com>
2022-02-23 17:44:18 -0500

    Bcrypt and login route

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,4 +1,4 @@
 const router = require('express').Router();
 const userRoutes = require('./user-routes.js');
-router.use('/api/users', userRoutes);
+router.use('/users', userRoutes);
 module.exports = router;
--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -31,6 +31,7 @@ router.get('/:id', (req, res) => {
 });
 router.put('/:id', (req, res) => {
     User.update(req.body, {
+        individualHooks: true,
         where: {
             id: req.params.id
         }
@@ -57,6 +58,25 @@ router.post('/', (req, res) => {
             console.log(err);
             res.status(500).json(err);
         })
+})
+router.post('/login', (req, res) => {
+    User.findOne({
+        where: {
+            email: req.body.email
+        }
+    }).then(dbUserData => {
+        if (!dbUserData) {
+            res.status(400).json({ message: 'No user with that email.' });
+            return;
+        }
+        //res.json({user: dbUserData});
+        const validPassword = dbUserData.checkPassword(req.body.password);
+        if (!validPassword) {
+            res.status(400).json({ message: 'Incorrect Password' });
+            return;
+        }
+        res.json({ user: dbUserData, message: 'You are now logged in' });
+    })
 })
 router.delete('/:id', (req, res) => {
     User.destroy({
